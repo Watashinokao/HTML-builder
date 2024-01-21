57cd4ef51957dab779f7a86d48b4dd6d46c7e1c8
Darya <watashi.no.kao24@gmail.com>
2024-01-21 20:58:44 +0300

    feat: add script for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+const myProcess = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const myStream = fs.WriteStream(path.join(__dirname, 'text.txt'), {
+  encoding: 'utf8',
+});
+myProcess.setPrompt('\u001b[34m' + 'You can write here: ' + '\u001b[0m');
+myProcess.prompt();
+myProcess.on('line', (text) => {
+  if (text.trim() === 'exit') {
+    myProcess.close();
+  } else {
+    myStream.write(text + '\n');
+    myProcess.prompt();
+  }
+});
+myProcess.on('close', () => {
+  myProcess.setPrompt('\u001b[32m' + 'Buy! Have a good day!' + '\u001b[0m');
+  myProcess.prompt();
+});
+myProcess.on('error', (error) => {
+  console.error('\u001b[31m' + error + '\u001b[0m');
+});
+myStream.on('error', (error) => {
+  console.error('\u001b[31m' + error + '\u001b[0m');
+});
